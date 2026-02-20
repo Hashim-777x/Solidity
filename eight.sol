0451c5061610b3785939db44e1c4251b1daa6339
Hashim-777x <syedhashim721@gmail.com>
2026-02-20 15:47:27 +0530

    changes

--- eight.sol
+++ eight.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract SM_global{
+    
 string [] public names = ["Alice", "Bob", "Charlie", "David"];
 //memory is temp, not stored in contract and it makes copy 
 function setNameMemory(uint _index, string memory value) public view{// can use view coz it is copy array of Org
@@ -18,5 +19,4 @@ function setNameStorage(uint _index, string memory value) public {// can't use v
 function info() public view  returns (uint  , uint  , address ){ // (uint -> blockno , uint -> timestamp , address -> msgSender)
 return ( block.number, block.timestamp, msg.sender);
 }
-
 }
