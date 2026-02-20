107150c4476a72b4859e08980d6a0dbed70eb2d5
Hashim-777x <syedhashim721@gmail.com>
2026-02-20 15:12:18 +0530

    updates

--- nine.sol
+++ nine.sol
@@ -18,28 +18,3 @@ contract Pay {
     }
 }
 
-// contract Pay {
-//     // Ensure the address is correct for the network you are on (Goerli, Sepolia, etc.)
-//     address payable public user = payable(0xCD28E737097af95A1F4c00AC7837904708164F2d);
-
-//     // This allows the contract to receive ETH directly
-//     receive() external payable {}
-
-//     function PayCoins() public payable {
-//         // This function stays empty just to accept funds via the 'Value' field
-//     }
-
-//     function getBalance() public view returns(uint) {
-//         return address(this).balance;
-//     }
-
-//     function sendCoinsAccounts() public {
-//        // uint amount = 0.0005 ether;
-        
-//         // Safety check: ensure the contract has enough balance
-//      //   require(address(this).balance >= amount, "Contract has insufficient funds");
-
-//         // (bool success, ) = user.call{value: amount}("");
-//         // require(success, "Transfer failed.");
-//     }
-// }
